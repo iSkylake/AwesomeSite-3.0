1eb69ad8eb863747fb538366212b174e418f6869
iSkylake <skylakebeta@gmail.com>
2018-07-05 14:21:49 -0700

    Add projects overlay

--- main.js
+++ main.js
@@ -10,18 +10,18 @@ window.onscroll = function(){
 	if(window.pageYOffset >= navPosition){
 		navBar.classList.add('fixedNav');
 		about.style.paddingTop = "153px";
-		skillAnimation();
+		aboutAnimation();
 	} else {
 		navBar.classList.remove('fixedNav');
 		about.style.paddingTop = "90px";
 	}
 };
 
-function skillAnimation(){
+function aboutAnimation(){
 	let progress = ["75%", "50%", "65%", "40%", "45%", "40%", "60%", "35%", "55%"];
 	// let seconds = 1;
-	about.children[0].style.transform = "translateY(0)";
-	about.children[0].style.opacity = 1;
+	// about.children[0].style.transform = "translateY(0)";
+	// about.children[0].style.opacity = 1;
 	about.children[1].style.transform = "translate(0)";
 	about.children[1].style.opacity = 1;
 	about.children[2].style.transform = "translate(0)";
