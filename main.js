95d59841250cc50df4fd7da572886909ecefd8b6
iSkylake <skylakebeta@gmail.com>
2018-07-04 18:39:45 -0700

    Add About section and fix navbar scroll fixed positioning

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 let navBar = document.getElementsByTagName('nav')[0];
 let downIcon = document.getElementsByClassName('down-icon')[0];
+let about = document.getElementById('about');
+let skillProgression = document.getElementsByClassName('skill-progression');
 
 // let documentScroll = document.documentElement || document.body;
 let navPosition = navBar.offsetTop;
@@ -7,10 +9,20 @@ let navPosition = navBar.offsetTop;
 window.onscroll = function(){
 	if(window.pageYOffset >= navPosition){
 		navBar.classList.add('fixedNav');
+		about.style.paddingTop = "153px";
+		skillAnimation();
 	} else {
-		navBar.classList.remove('fixedNav')
+		navBar.classList.remove('fixedNav');
+		about.style.paddingTop = "90px";
 	}
 };
+
+function skillAnimation(){
+	let progress = ["75%", "50%", "65%", "40%", "45%", "40%", "60%", "35%", "55%"];
+	for(let i=0; i<skillProgression.length; i++){
+		skillProgression[i].style.width = progress[i];
+	}
+}
 
 const scrollTo = function(to, duration) {
 	let
