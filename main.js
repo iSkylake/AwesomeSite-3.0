0ca8d08334f4e74440c090b58cd314b942422b0b
iSkylake <skylakebeta@gmail.com>
2018-07-08 07:24:08 -0700

    Add animation transition to portfolio

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ let navBar = document.getElementsByTagName('nav')[0];
 let downIcon = document.getElementsByClassName('down-icon')[0];
 let about = document.getElementById('about');
 let skillProgression = document.getElementsByClassName('skill-progression');
+let portfolio = document.getElementById('portfolio');
+let projectOverlay = document.getElementsByClassName('project-overlay');
 
 // let documentScroll = document.documentElement || document.body;
 let navPosition = navBar.offsetTop;
+let portfolioPosition = portfolio.offsetTop - 20;
 
 window.onscroll = function(){
 	if(window.pageYOffset >= navPosition){
@@ -14,6 +17,10 @@ window.onscroll = function(){
 	} else {
 		navBar.classList.remove('fixedNav');
 		about.style.paddingTop = "90px";
+	}
+
+	if(window.pageYOffset >= portfolioPosition) {
+		portfolioAnimation();
 	}
 };
 
@@ -30,6 +37,14 @@ function aboutAnimation(){
 		// seconds += 0.05;
 		skillProgression[i].style.width = progress[i];
 		// skillProgression[i].style.transitionDuration = seconds + "s";
+	}
+}
+
+function portfolioAnimation(){
+	portfolio.children[0].style.opacity = 1;
+	for(let i=0; i<projectOverlay.length; i++){
+		projectOverlay[i].style.transform = "rotateX(0deg)";
+		projectOverlay[i].style.opacity = 1;
 	}
 }
 
@@ -64,3 +79,4 @@ const scrollTo = function(to, duration) {
 };
 
 downIcon.addEventListener('click', () => scrollTo(navPosition, 500));
+
